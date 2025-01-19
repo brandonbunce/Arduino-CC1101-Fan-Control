ffa0e83ac9c12ef72549e58ee88f16e67d29e0de
Brandon Bunce <67491262+brandonbunce@users.noreply.github.com>
2025-01-19 10:10:32 -0800

    Create credentials.h

---
+++ main/credentials.h
@@ -0,0 +1,9 @@
+// Define wifi settings
+#define WIFI_SSID "SSID"       // your network SSID (name)
+#define WIFI_PASS "PASSWORD"  // your network password (use for WPA, or use as key for WEP)
+
+// Define MQTT broker settings
+#define MQTT_HOST "XXX.XXX.XXX.XXX"
+#define MQTT_PORT XXXX // usually 1883
+#define MQTT_USER "USERNAME"
+#define MQTT_PASS "PASSWORD"
